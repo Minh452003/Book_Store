8dd0fe0bc4b627d5bae9cea080419133cf89c4ec
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-09-17 19:45:33 +0700

    feat: create upload

--- BE/src/app.js
+++ BE/src/app.js
@@ -6,6 +6,7 @@ import routerProducts from "./routers/products.js";
 import routerCategory from "./routers/category.js";
 import routerAuth from "./routers/auth.js";
 import routerUser from "./routers/user.js";
+import uploadRouter from "./routers/upload.js";
 
 import cookieParser from "cookie-parser";
 
@@ -21,6 +22,7 @@ app.use("/api", routerProducts);
 app.use("/api", routerCategory);
 app.use("/api", routerAuth);
 app.use("/api", routerUser);
+app.use("/api", uploadRouter);
 
 app.listen(8080, async () => {
     await mongoose.connect(process.env.URL_MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
